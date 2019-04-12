e478346a9a1caec52e962a53187a5c56b067ad30
James Collins <jbccollins@gmail.com>
2019-04-11 22:10:15 -0400

    Refactor to bag class

--- Assets/Bag.cs
+++ Assets/Bag.cs
@@ -17,24 +17,34 @@ namespace Application
         private Quaternion OriginalCameraRotation = Quaternion.Euler(new Vector3(20, 0, 0));
         public Bag()
         {
-        }
-
-        void Start()
-        {
             Gyro = new Vector3(0, 0, 0);
             Accelleration = new Vector3(0, 0, 0);
-            Rigidbody = new Rigidbody();
+        }
+
+        private void Start()
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.localScale = new Vector3(0.5f, 0.2f, 0.5f);
+            cube.AddComponent<BoxCollider>();
+            cube.AddComponent<Rigidbody>();
+
+            //cube.GetComponent<BoxCollider>().material = BagPhysicsMaterial;
+            Rigidbody = cube.GetComponent<Rigidbody>();
+            Rigidbody.mass = 1.5f;
+            Rigidbody.drag = 0;
+            Rigidbody.angularDrag = 0.05f;
+            Rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
             Reset();
         }
 
-        private void Reset()
+        public void Reset()
         {
             Rigidbody.useGravity = false;
             Rigidbody.transform.position = OriginalPosition;
             Rigidbody.transform.rotation = OriginalRotation;
             Rigidbody.velocity = OriginalVelocity;
             Rigidbody.angularVelocity = OriginalVelocity;
-            Rigidbody.transform.rotation = OriginalCameraRotation;
+            //Rigidbody.transform.rotation = OriginalCameraRotation;
         }
 
         public void Throw(Joycon j)
@@ -48,6 +58,51 @@ namespace Application
             float forwardForce = System.Math.Abs(Accelleration.z);
             Vector3 force = new Vector3(0, upForce, forwardForce) * thrust;
             Rigidbody.AddForce(force);
+        }
+        public void Move(bool left)
+        {
+            Vector3 newPosition = new Vector3(Rigidbody.transform.position.x + (0.05f * (left ? 1.0f : -1.0f)), Rigidbody.transform.position.y, Rigidbody.transform.position.z);
+            Rigidbody.transform.position = newPosition;
+        }
+        public void Rotate(float stickX)
+        {
+            Debug.Log(string.Format("Stick x: {0:N}", stickX));
+            // float speed = 1.0f * (stickX > 0 ? 1.0f : -1.0f);
+            // Quaternion newBagRotation = Quaternion.Euler(new Vector3(bagRididBody.transform.rotation.x, bagRididBody.transform.rotation.y + speed, bagRididBody.transform.rotation.z));
+            // Quaternion newCameraRotation = Quaternion.Euler(new Vector3(20, mainCamera.transform.rotation.y + speed, mainCamera.transform.rotation.z));
+
+            //bagRididBody.transform.rotation = newBagRotation;
+            Vector3 rot = Vector3.up * (stickX > 0 ? 1 : -1);
+            Rigidbody.transform.Rotate(rot);
+            //mainCamera.transform.Rotate(rot);
+            //Follow();
+        }
+        // Position camera to follow behind player's head.
+        //private void Follow(){
+
+        //  // orientation as an angle when projected onto the XZ plane
+        //  // this functionality is modularise into a separate method because
+        //  // I use it elsewhere
+        //  float playerAngle = AngleOnXZPlane (bagRididBody.transform);
+        //  float cameraAngle = AngleOnXZPlane (mainCamera.transform);
+
+        //  // difference in orientations
+        //  float rotationDiff = Mathf.DeltaAngle(cameraAngle, playerAngle);
+
+        //  // rotate around target by time-sensitive difference between these angles
+        //  mainCamera.transform.RotateAround(bagRididBody.transform.position, Vector3.up, rotationDiff * Time.deltaTime);
+        //}
+
+        // Find the angle made when projecting the rotation onto the xz plane.
+        // You could pass in the rotation as a parameter instead of the transform.
+        private float AngleOnXZPlane(Transform item)
+        {
+
+            // get rotation as vector (relative to parent)
+            Vector3 direction = item.rotation * new Vector3(1, 1, 1);
+
+            // return angle in degrees when projected onto xz plane
+            return Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
         }
 
     }
--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -3,119 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameState : MonoBehaviour {
-	public Transform activeBag;
+	public Application.Bag ActiveBag;
 	public Transform mainCamera;
 
 	private List<Joycon> joycons;
-	public float thrust = 200.0f;
-
     // Values made available via Unity
     public float[] stick;
-    public Vector3 gyro;
-    public Vector3 accel;
     public int jc_ind = 0;
-    public Quaternion orientation;
-	private Rigidbody bagRididBody;
-	private Quaternion originalRotation = Quaternion.identity;
-	private Vector3 originalVelocity = Vector3.zero;
-	private Vector3 originalPosition = new Vector3(0, 2.0f, -15.0f);
-	private Quaternion originalCameraRotation = Quaternion.Euler(new Vector3(20, 0, 0));
     private List<Rigidbody> thrownBags = new List<Rigidbody>();
+    private int round;
 
-	private void reset () {
-		bagRididBody.useGravity = false;
-		bagRididBody.transform.position = originalPosition;
-		bagRididBody.transform.rotation = originalRotation;
-		bagRididBody.velocity = originalVelocity;
-		bagRididBody.angularVelocity = originalVelocity;
-		mainCamera.transform.rotation = originalCameraRotation;
-	}
-
-    private void CloneBag () {
-        thrownBags.Add(bagRididBody);
-        Rigidbody newBagRigidBody = Instantiate(bagRididBody);
-        newBagRigidBody.useGravity = false;
-        newBagRigidBody.transform.position = originalPosition;
-        newBagRigidBody.transform.rotation = originalRotation;
-        newBagRigidBody.velocity = originalVelocity;
-        newBagRigidBody.angularVelocity = originalVelocity;
-        mainCamera.transform.rotation = originalCameraRotation;
-        bagRididBody = newBagRigidBody;
-    }
-
-	private void throwBag (Joycon j) {
-		bagRididBody.useGravity = true;
-		//Debug.Log (transform.forward);
-		accel = j.GetAccel();
-		
-		Debug.Log (accel);
-		// Joycon x axis is the same as Unity's y axis
-		float upForce = System.Math.Abs(accel.x);
-		// Joycon z axis is the same as Unity's z axis
-		float forwardForce = System.Math.Abs(accel.z);
-		Vector3 force = new Vector3(0, upForce, forwardForce) * thrust;
-		bagRididBody.AddForce(force);
-	}
-
-	private void moveBag(bool left) {
-		Vector3 newPosition = new Vector3(bagRididBody.transform.position.x + (0.05f * (left ? 1.0f : -1.0f)), bagRididBody.transform.position.y, bagRididBody.transform.position.z);
-		bagRididBody.transform.position = newPosition;
-	}
-
-	// Position camera to follow behind player's head.
-	private void Follow(){
-
-		// orientation as an angle when projected onto the XZ plane
-		// this functionality is modularise into a separate method because
-		// I use it elsewhere
-		float playerAngle = AngleOnXZPlane (bagRididBody.transform);
-		float cameraAngle = AngleOnXZPlane (mainCamera.transform);
-
-		// difference in orientations
-		float rotationDiff = Mathf.DeltaAngle(cameraAngle, playerAngle);
-
-		// rotate around target by time-sensitive difference between these angles
-		mainCamera.transform.RotateAround(bagRididBody.transform.position, Vector3.up, rotationDiff * Time.deltaTime);
-	}
-
-	// Find the angle made when projecting the rotation onto the xz plane.
-	// You could pass in the rotation as a parameter instead of the transform.
-	private float AngleOnXZPlane(Transform item){
-
-		// get rotation as vector (relative to parent)
-		Vector3 direction = item.rotation * new Vector3(1, 1, 1);
-
-		// return angle in degrees when projected onto xz plane
-		return Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-	}
-
-	private void rotateBag(float stickX) {
-		Debug.Log(string.Format("Stick x: {0:N}",stickX));
-		// float speed = 1.0f * (stickX > 0 ? 1.0f : -1.0f);
-		// Quaternion newBagRotation = Quaternion.Euler(new Vector3(bagRididBody.transform.rotation.x, bagRididBody.transform.rotation.y + speed, bagRididBody.transform.rotation.z));
-		// Quaternion newCameraRotation = Quaternion.Euler(new Vector3(20, mainCamera.transform.rotation.y + speed, mainCamera.transform.rotation.z));
-		
-		//bagRididBody.transform.rotation = newBagRotation;
-		Vector3 rot = Vector3.up * (stickX > 0 ? 1 : -1);
-		bagRididBody.transform.Rotate(rot);
-		//mainCamera.transform.Rotate(rot);
-		Follow();
-	}
+	//private void reset () {
+	//	mainCamera.transform.rotation = originalCameraRotation;
+	//}
 
     void Start ()
     {
-        gyro = new Vector3(0, 0, 0);
-        accel = new Vector3(0, 0, 0);
-		bagRididBody = activeBag.GetComponent<Rigidbody>();
-		bagRididBody.useGravity = false;
-
         // get the public Joycon array attached to the JoyconManager in scene
         joycons = JoyconManager.Instance.j;
 		if (joycons.Count < jc_ind+1){
-			Destroy(gameObject);
+            Debug.LogError("No Joycon found");
 		}
-		reset();
-	}
+        ActiveBag = gameObject.AddComponent<Application.Bag>();
+    }
 
 	void FixedUpdate () {
 		if (joycons.Count > 0)
@@ -123,23 +33,20 @@ public class GameState : MonoBehaviour {
 			Joycon j = joycons [jc_ind];
 			if (j.GetButtonUp (Joycon.Button.SHOULDER_2))
 			{
-				throwBag(j);
+                ActiveBag.Throw(j);
 			}
 			if (j.GetButtonDown(Joycon.Button.DPAD_UP)) {
-                //reset();
-                Debug.Log("DPAD_UP");
-                CloneBag();
-
+                ActiveBag.Reset();
             }
 			if (j.GetButton(Joycon.Button.DPAD_LEFT)) {
-				moveBag(false);
+                ActiveBag.Move(false);
 			}
 			if (j.GetButton(Joycon.Button.DPAD_RIGHT)) {
-				moveBag(true);
+                ActiveBag.Move(true);
 			}
 			if (j.GetButtonDown(Joycon.Button.DPAD_DOWN)) {
-				Debug.Log(string.Format("Stick x: {0:N} Stick y: {1:N}",j.GetStick()[0],j.GetStick()[1]));
-			}
+                ActiveBag = gameObject.AddComponent<Application.Bag>();
+            }
 			// if (j.GetButton(Joycon.Button.STICK)) {
 			// 	var stickX = j.GetStick()[0];
 			// 	if (Mathf.Abs(stickX) > 0.2) {
@@ -163,7 +70,7 @@ public class GameState : MonoBehaviour {
 			}
 			var stickX = j.GetStick()[0];
 			if (Mathf.Abs(stickX) > 0.2) {
-				rotateBag(stickX);
+                ActiveBag.Rotate(stickX);
 			}
         }
     }
